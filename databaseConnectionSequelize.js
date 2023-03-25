376d9bc5ec670f2629e2a5683452486980d5e03a
Jun <junability@gmail.com>
2023-03-24 18:17:03 -0700

    second

---
+++ databaseConnectionSequelize.js
@@ -0,0 +1,10 @@
+const is_qoddi = process.env.IS_QODDI || false;
+const dbConfigQoddi = "freedb_junissexy:D2QGXfPpU3aq*aY@sql.freedb.tech/freedb_comp2350-week4-A01335052";
+const dbConfigLocal = "mysql://root:jchoi2022@@localhost/week10";
+if (is_qoddi) {
+var databaseConnectionString = dbConfigQoddi;
+}
+else {
+var databaseConnectionString = dbConfigLocal;
+}
+module.exports = databaseConnectionString;
