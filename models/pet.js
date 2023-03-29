fd7d2ed67c7c5a01025d471ccf119f6fff6b3a1f
Jun <junability@gmail.com>
2023-03-29 11:00:59 -0700

    commit

--- models/pet.js
+++ models/pet.js
@@ -1,16 +1,29 @@
-const {Sequelize, DataTypes} = require('sequelize');
-const databaseConnectionString = require('../databaseConnectionSequelize');
+const { Sequelize, DataTypes } = require("sequelize");
+const databaseConnectionString = require("../databaseConnectionSequelize");
 const sequelize = new Sequelize(databaseConnectionString);
-const petModel = sequelize.define('pet',
-{pet_id: {type: DataTypes.INTEGER, autoIncrement: true,
-primaryKey: true},
-web_user_id: {type: DataTypes.INTEGER, allowNull: false},
-name: {type: DataTypes.STRING, allowNull: false},
-pet_type_id: {type: DataTypes.INTEGER, allowNull: false, foreignKey: true}
-},
-{tableName: 'pet',
-timestamps: false,
-singular: 'pet',
-plural: 'pet'}
+const petModel = sequelize.define(
+  "pet",
+  {
+    pet_id: { type: DataTypes.INTEGER, autoIncrement: true, primaryKey: true },
+    web_user_id: { type: DataTypes.INTEGER, allowNull: false },
+    name: { type: DataTypes.STRING, allowNull: false },
+    pet_type_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      foreignKey: true,
+    },
+  },
+  { tableName: "pet", timestamps: false, singular: "pet", plural: "pet" }
 );
+const userModel = include("models/web_user");
+petModel.belongsTo(userModel, {
+  as: "owner",
+  timestamps: false,
+  foreignKey: "web_user_id",
+});
+userModel.hasMany(petModel, {
+  as: "pets",
+  timestamps: false,
+  foreignKey: "web_user_id",
+});
 module.exports = petModel;
